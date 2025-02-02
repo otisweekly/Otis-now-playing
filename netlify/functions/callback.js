30650f55a7ddedb9e4a739468dca1f9892667e22
otisweekly <elliot.m.hasse@gmail.com>
2025-02-02 00:08:28 -0700

    Update callback.js

--- netlify/functions/callback.js
+++ netlify/functions/callback.js
@@ -9,16 +9,17 @@ exports.handler = async function(event, context) {
       <html>
         <head>
           <title>Spotify Authorization</title>
-        </head>
-        <body>
           <script>
             if (window.location.hash) {
               const token = new URLSearchParams(window.location.hash.substring(1)).get('access_token');
-              window.opener.postMessage({ token: token }, '*');
-              window.close();
+              // Store token and redirect back to main site
+              localStorage.setItem('spotify_token', token);
+              window.location.href = 'https://otisweekly.com'; // Replace with your actual domain
             }
           </script>
-          <p>Authorization successful! You can close this window.</p>
+        </head>
+        <body>
+          <p>Authorizing...</p>
         </body>
       </html>
     `
