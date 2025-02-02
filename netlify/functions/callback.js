6e8906a072bf01726a5b760ef5bfba2f2b4982a4
otisweekly <elliot.m.hasse@gmail.com>
2025-02-01 19:47:32 -0700

    Update callback.js

--- netlify/functions/callback.js
+++ netlify/functions/callback.js
@@ -2,19 +2,25 @@ exports.handler = async function(event, context) {
   return {
     statusCode: 200,
     headers: {
-      'Content-Type': 'text/html',
-      'Access-Control-Allow-Origin': '*'
+      'Content-Type': 'text/html'
     },
     body: `
       <html>
         <body>
           <script>
+            // Get the token from the URL hash
             if (window.location.hash) {
               const token = new URLSearchParams(window.location.hash.substring(1)).get('access_token');
-              window.opener.postMessage({ token }, '*');
-              window.close();
+              localStorage.setItem('spotify_token', token);
+              if (window.opener) {
+                window.opener.postMessage({ token }, '*');
+                window.close();
+              } else {
+                window.location.href = '/';
+              }
             }
           </script>
+          <p>Authorization successful! You can close this window.</p>
         </body>
       </html>
     `
